6a9275e7877f0a3afef594cab1fdec88f5c345e5
Vixentael <vixentael@gmail.com>
2014-08-07 12:36:01 +0300

    update install.sh file to point on appcode30 path; add filetemplates

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 echo "This script is installing Color schemes, Live Templates and CodeStyles for Appcode"
 
-APPCODE_PREFS_DIR=~/Library/Preferences/appCode20
+APPCODE_PREFS_DIR=~/Library/Preferences/appCode30
 
 echo  "Copying files to ${APPCODE_PREFS_DIR}"
 
 cp -R codestyles ${APPCODE_PREFS_DIR}
 cp -R colors ${APPCODE_PREFS_DIR}
 cp -R templates ${APPCODE_PREFS_DIR}
+cp -R fileTemplates ${APPCODE_PREFS_DIR}
+
 
 echo "Done"
